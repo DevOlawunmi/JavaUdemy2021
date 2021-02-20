38159c7d555f083efe2860addda0310a3cde4bd8
Ola <olawunmi263@yahoo.com>
2021-02-20 05:51:08 +0000

    Intro material

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
-
+            int myFirstNumber= 5;
+        //System.out.println(myFirstNumber);
+       // myFirstNumber=87;
+       int mySecondNumber = 40;
+       int myThirdNumber = 37;
+       int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = myTotal-1000;
+        System.out.println(myLastOne);
     }
 }
